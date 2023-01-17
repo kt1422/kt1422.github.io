cd5f3fa5da85fb74cf2671b4b610879ca42de599
Kenneth Tan <kennethtan1414@gmail.com>
2023-01-17 11:42:08 +0800

    medicine and diagnosis

--- js/chart-bar-demo.js
+++ js/chart-bar-demo.js
@@ -5,8 +5,12 @@ Chart.defaults.global.defaultFontColor = '#292b2c';
 // Bar Chart Example
 var chart1 = document.getElementById("myBarChart");
 var chart2 = document.getElementById("myBarChart2");
+var chart3 = document.getElementById("myBarChart3");
+var chart4 = document.getElementById("myBarChart4");
 passValue(chart1);
 passValue(chart2);
+passValue2(chart3);
+passValue3(chart4);
 function passValue(ctx){
   var myLineChart = new Chart(ctx, {
     type: 'bar',
@@ -36,6 +40,92 @@ function passValue(ctx){
           ticks: {
             min: 0,
             max: 500,
+            maxTicksLimit: 5
+          },
+          gridLines: {
+            display: true
+          }
+        }],
+      },
+      legend: {
+        display: false
+      }
+    }
+  });
+}
+
+function passValue2(ctx){
+  var myLineChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ["Biogesic", "Neozep", "Diatab", "Allerta", "Bandage", "Solmux"],
+      datasets: [{
+        label: "Students",
+        backgroundColor: "rgba(2,117,216,1)",
+        borderColor: "rgba(2,117,216,1)",
+        data: [312, 210, 160, 102, 60, 48],
+      }],
+    },
+    options: {
+      scales: {
+        xAxes: [{
+          time: {
+            unit: 'month'
+          },
+          gridLines: {
+            display: false
+          },
+          ticks: {
+            maxTicksLimit: 6
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            min: 0,
+            max: 500,
+            maxTicksLimit: 5
+          },
+          gridLines: {
+            display: true
+          }
+        }],
+      },
+      legend: {
+        display: false
+      }
+    }
+  });
+}
+
+function passValue3(ctx){
+  var myLineChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ["Biogesic", "Neozep", "Diatab", "Allerta", "Bandage", "Solmux"],
+      datasets: [{
+        label: "Students",
+        backgroundColor: "rgba(2,117,216,1)",
+        borderColor: "rgba(2,117,216,1)",
+        data: [28, 49, 60, 82, 90, 128],
+      }],
+    },
+    options: {
+      scales: {
+        xAxes: [{
+          time: {
+            unit: 'month'
+          },
+          gridLines: {
+            display: false
+          },
+          ticks: {
+            maxTicksLimit: 6
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            min: 0,
+            max: 200,
             maxTicksLimit: 5
           },
           gridLines: {
