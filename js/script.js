1289ce5367285dbbcb00c14c40d37037655d566c
Kenneth Tan <kennethtan1414@gmail.com>
2023-01-18 20:29:10 +0800

    ready

--- js/script.js
+++ js/script.js
@@ -15,3 +15,26 @@ if (window.matchMedia('screen and (max-width: 768px)').matches) {
     const myCollapsible = new bootstrap.Collapse("#collapseWidthExample");
     myCollapsible.hide();
 }
+
+$("#rowAdder").click(function () {
+    newRowAdd =
+    '<div id="row" class="row d-flex justify-content-between"><div class="col-12 col-md-8 col-lg-9 pe-3"><div class="input-group mb-2">' +
+    '<button class="btn btn-danger" id="DeleteRow" type="button">' +
+    '<i class="bi bi-trash"></i>&nbsp;&nbsp;Delete</button>' +
+    '<select class="form-select">'+
+    '<option selected>Allerta</option>'+
+    '<option value="1">Bioflu</option>'+
+    '<option value="2">Biogesic</option>'+
+    '</select></div></div>'+
+    '<div class="col-6 col-md-4 col-lg-3"><div class="input-group mb-2">'+
+    '<span class="input-group-text" id="basic-addon1">Qty.</span>'+
+    '<input type="number" value="1" class="form-control">'+
+    '</div></div>'+
+    '</div>';
+
+    $('#newinput').append(newRowAdd);
+});
+
+$("body").on("click", "#DeleteRow", function () {
+    $(this).parents("#row").remove();
+})
